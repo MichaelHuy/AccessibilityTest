892ac61855f450cd24c833be43d254a352364391
Huy Le <lehuydit@gmail.com>
2021-02-22 21:33:20 +0700

    first commit

---
+++ app/src/main/java/com/identiv/accesstest/GlobalActionBarService.java
@@ -0,0 +1,49 @@
+package com.identiv.accesstest;
+
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.GestureDescription;
+import android.graphics.Path;
+import android.graphics.PixelFormat;
+import android.media.AudioManager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class GlobalActionBarService extends AccessibilityService {
+    FrameLayout mLayout;
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        // Create an overlay and display the action bar
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mLayout = new FrameLayout(this);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY;
+        lp.format = PixelFormat.TRANSLUCENT;
+        lp.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.gravity = Gravity.TOP;
+        LayoutInflater inflater = LayoutInflater.from(this);
+        inflater.inflate(R.layout.action_bar, mLayout);
+        wm.addView(mLayout, lp);
+    }
+}
